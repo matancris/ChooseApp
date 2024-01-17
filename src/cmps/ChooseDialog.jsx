64fe8e2e18b5eb01659fe4f7b811e64aaf960d47
Matan Crispel <matan.cris@gmail.com>
2024-01-17 20:13:45 +0200

    added choose modal

---
+++ src/cmps/ChooseDialog.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import { utilService } from "../services/util-service.js";
+
+export default function ChooseDialog({ person }) {
+
+  const [currPref, setCurrPref] = useState('')
+  const [isIntervalFinished, setIsIntervalFinished] = useState(false)
+
+  useEffect(() => {
+    if (person.preferences) {
+      restartInterval()
+    }
+  }, [person])
+
+  useEffect(() => {
+    console.log('useEffect ~ isIntervalFinished:', isIntervalFinished)
+    if (!isIntervalFinished) {
+      restartInterval()
+    }
+  }, [isIntervalFinished])
+
+  const restartInterval = () => {
+
+    // setIsIntervalFinished(() => false);
+
+    let loaderInterval = setInterval(() => {
+      setTimeout(() => {
+        clearInterval(loaderInterval)
+        loaderInterval && setIsIntervalFinished(true);
+        loaderInterval = null
+      }, 5000)
+      getRandomPref()
+    }, 300);
+
+
+  }
+
+  const getRandomPref = () => {
+    const randIdx = utilService.getRandomInt(0, person.preferences?.length);
+    setCurrPref(person.preferences?.[randIdx]);
+  }
+
+  return (
+    <section className={`choose-dialog flex justify-align-center column ${isIntervalFinished ? 'chosen space-between': ''}`}>
+      <h1>
+        {currPref}
+      </h1>
+      {isIntervalFinished && <button onClick={() => setIsIntervalFinished(false)}>נסיון אחרון!</button>}
+    </section>
+  )
+}
--- src/pages/PersonDetails.jsx
+++ src/pages/PersonDetails.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from 'react'
+import { useEffect, useState, useContext } from 'react';
 import { WEEK_DAYS, personService } from '../services/person-service';
 import { utilService } from '../services/util-service';
 import { Link, useParams } from 'react-router-dom';
@@ -8,101 +8,138 @@ import 'react-tabs/style/react-tabs.css';
 import AppDialog from '../cmps/app-cmps/AppDialog';
 import PersonEdit from '../cmps/PersonEdit';
 import PersonWeekEdit from '../cmps/PersonWeekEdit';
+import ChooseDialog from '../cmps/ChooseDialog';
 
 // TODO: no need to send personId from the child in every action, the person Id exist in this component - fix it.
 
 export default function PersonDetails() {
-    // const isMobile = useContext(MobileContext);
-    // console.log('PersonDetails ~ isMobile:', isMobile)
-    const [person, setPerson] = useState({});
-    const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
-    const [isWeekEditDialogOpen, setIsWeekEditDialogOpen] = useState(false);
-    const { personId } = useParams();
-    
-    
-    useEffect(() => {
-        loadPerson()
-    }, [])
+  // const isMobile = useContext(MobileContext);
+  // console.log('PersonDetails ~ isMobile:', isMobile)
+  const [person, setPerson] = useState({});
+  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
+  const [isWeekEditDialogOpen, setIsWeekEditDialogOpen] = useState(false);
+  const [isChooseLoaderOpen, setIsChooseLoaderOpen] = useState(false);
+  const { personId } = useParams();
 
-    const loadPerson = () => {
-        const currPerson = personService.getById(personId);
-        setPerson({ ...currPerson })
-    }
+  useEffect(() => {
+    loadPerson();
+  }, []);
 
-    const onAddPref = (personId, prefToAdd) => {
-        personService.addPersonPref(personId, prefToAdd)
-        loadPerson()
-    }
+  const loadPerson = () => {
+    const currPerson = personService.getById(personId);
+    setPerson({ ...currPerson });
+  };
 
-    const removePref = (personId, prefToRemoveIdx) => {
-        personService.removePersonPref(personId, prefToRemoveIdx)
-        loadPerson()
-    }
+  const onAddPref = (personId, prefToAdd) => {
+    personService.addPersonPref(personId, prefToAdd);
+    loadPerson();
+  };
 
-    const updatePrefs = (personId, updatedPrefs) => {
-        personService.updatePrefs(personId, updatedPrefs)
-        loadPerson()
-    }
+  const removePref = (personId, prefToRemoveIdx) => {
+    personService.removePersonPref(personId, prefToRemoveIdx);
+    loadPerson();
+  };
 
-    const updatePrefsByDays = (updatedPrefsByDays) => {
-        personService.updatePrefsByDays(personId, updatedPrefsByDays)
-        setIsWeekEditDialogOpen(false)
-        loadPerson()
-    }
+  const updatePrefs = (personId, updatedPrefs) => {
+    personService.updatePrefs(personId, updatedPrefs);
+    loadPerson();
+  };
 
+  const updatePrefsByDays = (updatedPrefsByDays) => {
+    personService.updatePrefsByDays(personId, updatedPrefsByDays);
+    setIsWeekEditDialogOpen(false);
+    loadPerson();
+  };
 
-    return (
-        <section className="person-details main-container">
-            <div className="person-details-header flex justify-align-center full">
-                <h1>{person.name}</h1>
-                <button className="back-btn flex justify-align-center">
-                    <Link to="/person">
-                        <span className="material-symbols-outlined">
-                            arrow_left_alt
-                        </span>
-                    </Link>
-                </button>
-            </div>
-            <div className={`personal-details-main flex`}>
-                <div className="side-nav flex column space-between">
-                    <ul className="prefs-container clean-list flex column">
-                        {person.preferences?.map((pref, idx) => <li key={idx}>{pref}</li>)}
-                    </ul>
-                    <button onClick={() => setIsEditDialogOpen(true)}>ערוך רשימה</button>
-                </div>
-                <Tabs className={'react-tabs flex-1 flex column space-between'} defaultIndex={utilService.getCurrDay()}>
-                    <TabList className={'react-tabs__tab-list flex'}>
-                        {WEEK_DAYS.map((day, idx) => <Tab className={'react-tabs__tab flex-1'} key={idx}>{day}</Tab>)}
-                    </TabList>
+  return (
+    <section className="person-details main-container">
+      <div className="person-details-header flex justify-align-center full">
+        <h1>{person.name}</h1>
+        <button className="back-btn flex justify-align-center">
+          <Link to="/person">
+            <span className="material-symbols-outlined">arrow_left_alt</span>
+          </Link>
+        </button>
+      </div>
+      <div className={`personal-details-main flex`}>
+        <div className="side-nav flex column space-between">
+          <ul className="prefs-container clean-list flex column">
+            {person.preferences?.map((pref, idx) => (
+              <li key={idx}>{pref}</li>
+            ))}
+          </ul>
+          <button onClick={() => setIsEditDialogOpen(true)}>ערוך רשימה</button>
+        </div>
+        <Tabs
+          className={'react-tabs flex-1 flex column space-between'}
+          defaultIndex={utilService.getCurrDay()}
+        >
+          <TabList className={'react-tabs__tab-list flex'}>
+            {WEEK_DAYS.map((day, idx) => (
+              <Tab className={'react-tabs__tab flex-1'} key={idx}>
+                {day}
+              </Tab>
+            ))}
+          </TabList>
 
-                    {WEEK_DAYS.map((_, dayIdx) => (
-                        <TabPanel key={dayIdx}>
-                            <h1 className="selection-title flex justify-align-center">
-                                {person.preferences?.[person.prefsByDays?.[dayIdx]]}
-                            </h1>
-                        </TabPanel>))}
-                        <button onClick={() => setIsWeekEditDialogOpen(true)}>ערוך את ימי השבוע</button>
-                </Tabs>
-            </div>
+          {WEEK_DAYS.map((_, dayIdx) => (
+            <TabPanel key={dayIdx}>
+              <h1 className="selection-title flex justify-align-center">
+                {person.preferences?.[person.prefsByDays?.[dayIdx]]}
+              </h1>
+            </TabPanel>
+          ))}
+          <div className="actions-container flex column gap-16">
+            <button
+              className="contrast-color"
+              onClick={() => setIsChooseLoaderOpen(true)}
+            >
+              בא לי משהו אחר!
+            </button>
+            <button onClick={() => setIsWeekEditDialogOpen(true)}>
+              ערוך את ימי השבוע
+            </button>
+          </div>
+        </Tabs>
+      </div>
 
-            {/* Edit list dialog */}
-            <AppDialog isDialogOpen={isEditDialogOpen} onCloseDialog={() => setIsEditDialogOpen(false)}>
-                <PersonEdit
-                    person={person}
-                    onAddPref={onAddPref}
-                    removePref={removePref}
-                    updatePrefs={updatePrefs} />
-            </AppDialog>
+      {/* DIALOGS */}
 
-            {/* Edit week dialog */}
-            <AppDialog isDialogOpen={isWeekEditDialogOpen} onCloseDialog={() => setIsWeekEditDialogOpen(false)}>
-                <PersonWeekEdit
-                    person={person} 
-                    updatePrefsByDays={updatePrefsByDays}
-                    weekDays={WEEK_DAYS}
-                    />
-            </AppDialog>
+      {/* Edit list dialog */}
+      <AppDialog
+        isDialogOpen={isEditDialogOpen}
+        onCloseDialog={() => setIsEditDialogOpen(false)}
+      >
+        <PersonEdit
+          person={person}
+          onAddPref={onAddPref}
+          removePref={removePref}
+          updatePrefs={updatePrefs}
+        />
+      </AppDialog>
 
-        </section>
-    )
+      {/* Edit week dialog */}
+      <AppDialog
+        isDialogOpen={isWeekEditDialogOpen}
+        onCloseDialog={() => setIsWeekEditDialogOpen(false)}
+      >
+        <PersonWeekEdit
+          person={person}
+          updatePrefsByDays={updatePrefsByDays}
+          weekDays={WEEK_DAYS}
+        />
+      </AppDialog>
+
+      {/* Choose loader Dialog */}
+      {isChooseLoaderOpen &&
+        <AppDialog
+          isDialogOpen={isChooseLoaderOpen}
+          onCloseDialog={() => setIsChooseLoaderOpen(false)}
+        >
+          <ChooseDialog
+            person={person}
+          />
+        </AppDialog>}
+    </section>
+  );
 }
