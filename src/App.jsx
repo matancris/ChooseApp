98905760f368980db16439de3705de61d0e46ec6
Matan Crispel <matan.cris@gmail.com>
2024-01-23 10:21:49 +0200

    base property

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import { useEffect, useState, useContext, createContext } from 'react'
 
 import ChooseApp from './pages/ChooseApp'
